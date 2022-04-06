49cdb001c6b1290bfe68f22d20630a0e7204735d
andreasvidal <andrea.s.vidal00@gmail.com>
2022-04-06 13:40:15 -0400

    registro con google funcionando

---
+++ .history/src/lib/views/home_20220406125546.js
@@ -0,0 +1,25 @@
+//import { dataUser } from "../../firebase/firebaseConfig.js"
+export const home = () => {
+    window.location.hash = '#/home';
+    const viewHome = document.createElement('div');
+    viewHome.classList.add("container-login");
+    const formHome = `<div class="containerLogin" id="containerLogin">
+    <img class="container-img" src="./img/logo-mobil.png" alt="logo BarterBook con un ícono del libro abierto" />
+    <h1 class="titleWelcome">¡Bienvenidos!</h1>
+    <div class="containerBtns">
+        <a href="#/login" class="links">Log In </a>
+        <a href="#/login" class="links">Sign Up with <img src="./img/google.png" alt="google" class="google"></a>
+        <a href="#/register" class="links">Register </a>
+    </div>
+</div>
+`;
+    viewHome.innerHTML += formHome;
+    //------------------BTN PARA INICIO SESION CON GOOGLE-------------------
+    viewHome
+        .querySelector('.google')
+        .addEventListener('click', () => {
+            checkGoogle();
+            //window.location.hash = "#/wall";
+        });
+    return viewHome
+};
---
+++ .history/src/lib/views/home_20220406132133.js
@@ -0,0 +1,27 @@
+import { checkGoogle } from "../../firebase/firebaseConfig.js"
+export const home = () => {
+    window.location.hash = '#/home';
+    const viewHome = document.createElement('div');
+    viewHome.classList.add("container-login");
+    const formHome = `<div class="containerLogin" id="containerLogin">
+    <img class="container-img" src="./img/logo-mobil.png" alt="logo BarterBook con un ícono del libro abierto" />
+    <h1 class="titleWelcome">¡Bienvenidos!</h1>
+    <div class="containerBtns">
+        <a href="#/login" class="links">Log In </a>
+        <a href="#/post" class="links">Sign Up with <img src="./img/google.png" alt="google" class="google"></a>
+        <a href="#/register" class="links">Register </a>
+    </div>
+</div>
+`;
+    viewHome.innerHTML += formHome;
+    //------------------BTN PARA INICIO SESION CON GOOGLE-------------------
+
+    const singGoogle = viewHome.querySelector(".google")
+    console.log(singGoogle)
+    singGoogle.addEventListener("click", async() => {
+        checkGoogle();
+        console.log(checkGoogle)
+            //window.location.hash = "#/wall";
+    });
+    return viewHome
+};
